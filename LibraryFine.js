c8cdb9a7ff4c29c3402780f3e226d122cdce143b
Ann <anna.muzykina83@gmail.com>
2019-06-11 11:33:24 +0300

    Update LibraryFine.js

--- LibraryFine.js
+++ LibraryFine.js
@@ -27,18 +27,19 @@ function readLine() {
 // Complete the libraryFine function below.
 function libraryFine(d1, m1, y1, d2, m2, y2) {
     
-  if (y1 > y2)
-    return 10000
-  else if (y2 > y1)
-    return 0
-  else if (m1 > m2)
-    return 500 * ((m1 - m2) + ((y1 - y2) * 12))
-  else if (m2 > m1)
-    return 0
-  else if (d1 > d2)
-    return 15 * (d1 - d2)
-  else
-    return 0
+  if (d1 > d2){
+        return 15 * (d1 - d2)
+    } else if (d1 <= 0){
+        return 0
+    } else if (m1 > m2){
+        return 500 * ((m1 -m2) + ((y1 - y2) * 12))
+    } else if ( m2 <= m1){
+        return 0
+    } else if (y1 > y2){
+        return 10000
+    } else{
+        return 0
+    }
 
 
 
